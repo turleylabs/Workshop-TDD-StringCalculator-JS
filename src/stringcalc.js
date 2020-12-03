d10b2375bc910e2ea182adaed011bc4ebb6b6157
Steve Turley <steve@turleylabs.com>
2020-12-03 08:27:40 -0600

    Initial commit

---
+++ src/stringcalc.js
@@ -0,0 +1,3 @@
+export function stringCalc(number) {
+    return null;
+}
---
+++ src/stringcalc.test.js
@@ -0,0 +1,7 @@
+import { stringCalc } from './stringcalc'
+
+test('RENAME ME', () => {
+    const expected = 0;
+    const actual = 0;
+    expect(expected).toEqual(actual);
+});
