487f7e382eb91baf7ecc146e43af52222a204f61
Steve Turley <steve@turleylabs.com>
2020-12-04 12:42:12 -0600

    Update to track test runs

--- src/stringcalc.test.js
+++ src/stringcalc.test.js
@@ -2,6 +2,6 @@ import { stringCalc } from './stringcalc'
 
 test('RENAME ME', () => {
     const expected = 0;
-    const actual = 0;
+    const actual = 1;
     expect(expected).toEqual(actual);
 });
